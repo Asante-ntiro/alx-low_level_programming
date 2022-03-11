1d615639cf26776c5168ca79ea98484311e06eeb
Asante-ntiro <asante.ntiro@gmail.com>
2022-03-11 14:53:56 +0300

    betty format

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,25 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+
 /**
- * main - Entry point
+ * main - Prints alphabet
+ * @void: Empty parameter list for main.
  *
- * Return: Always 0 (Success)
- */
+ * Description: Prints the alphabet in lower case
+ * Can only use putchar
+ *
+ * Return: 0 for success
+*/
 int main(void)
 {
+	char letter = 'a';
 
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-		putchar('\n');
-
-return (0);
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
 }
