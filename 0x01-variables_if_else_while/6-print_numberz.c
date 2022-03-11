0cf962647fe10006e0970778eca6008b32512b7b
Asante-ntiro <asante.ntiro@gmail.com>
2022-03-11 14:49:44 +0300

    updated 6-print_numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,24 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 /**
-* main - Print all single digit numbers using only putchar
-*
-* Return: Always 0 (Success)
+ * main - Prints base 10 numbers
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints numbers 0-9
+ *
+ * Return: 0 for success
 */
 int main(void)
 {
-	int digit;
+	int num = 48;
 
-	for (digit = 0; digit < 10; digit++)
-	putchar(digit + '0');
-
+	while (num <= 57)
+	{
+		putchar(num);
+		num++;
+	}
 	putchar('\n');
-
 	return (0);
 }
